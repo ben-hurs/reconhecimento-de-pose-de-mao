598005187b1142f6c03f7d9a9a6b1e22f0ac7f37
Ben-Hur Santana <besali96@gmail.com>
2024-03-16 13:55:58 -0300

    criação da pagina web

--- main.py
+++ main.py
@@ -1,48 +1,26 @@
+from flask import Flask, render_template, Response
 import cv2
-import time
-import numpy as np
-from cvzone.HandTrackingModule import HandDetector 
-import mediapipe as mp
-import math
+from reconhecimento_mao import videoCamera
 
-wCam, hCam = 1280, 720
+app = Flask(__name__)
 
-cap = cv2.VideoCapture(0)
-cap.set(3, wCam)
-cap.set(4, hCam)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-detector = HandDetector(detectionCon=0.7)
+def gen(reconhecimento_mao):
+    while True:
+        frame = reconhecimento_mao.get_frames()
+        yield (b'--frame\r\n'
+            b'Content-Type: image/jpeg\r\n\r\n' + frame 
+            + b'\r\n\r\n')
+        
+@app.route('/aplicacao')
+def aplicacao():
+    return Response(gen(videoCamera()),
+           mimetype='multipart/x-mixed-replace; boundary=frame')
 
-length = 0
-lengthPercentage = 0
 
-while True:
-    success, img = cap.read()
-    hands, img = detector.findHands(img, draw=True)
-    
-    if hands:
-        for hand in hands:
-            lmList = hand["lmList"]  # Lista de pontos chave da mão
-            if lmList:
-                x1, y1 = lmList[4][0], lmList[4][1]  
-                x2, y2 = lmList[8][0], lmList[8][1]  
-                cx, cy = (x1+x2)//2, (y1+y2)//2
 
-                cv2.circle(img, (x1, y1), 15, (255,0,255), cv2.FILLED)
-                cv2.circle(img, (x2, y2), 15, (255,0,255), cv2.FILLED)
-                cv2.line(img, (x1, y1), (x2, y2),(255,0,255),3)
-                cv2.circle(img, (cx, cy), 15, (255,0,255), cv2.FILLED)
-
-                length = int(math.hypot(x2-x1, y2-y1))
-                lengthPercentage = int((length / 400) * 100)
-
-                if length < 50:
-                    cv2.circle(img, (cx, cy), 15, (0,255,0), cv2.FILLED)
-
-    cv2.rectangle(img, (50, 200), (80, 600), (0,255,0), 3)
-    cv2.rectangle(img, (50, 600 - length), (80, 600), (0,255,0), cv2.FILLED)
-
-    cv2.putText(img, f'{lengthPercentage}%', (40, 650), cv2.FONT_HERSHEY_COMPLEX, 1, (255,0,0), 2)
-
-    cv2.imshow('Imagem',img)
-    cv2.waitKey(1)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
